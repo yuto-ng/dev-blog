db00b0d27ecc91ab105ac5575147fd8c9018662c
y.nagayoshi <y.nagayoshi@scene-live.com>
2023-12-02 16:32:59 +0900

    [add]定数ファイルを定義

---
+++ src/util/constants/size.ts
@@ -0,0 +1,4 @@
+export const ICON_SIZE = {
+    m: { height: '24px', width: '24px' },
+    s: { height: '16px', width: '16px' },
+};
--- src/util/type/Icon.ts
+++ src/util/type/Icon.ts
@@ -1,6 +1,10 @@
+import { ICON_SIZE } from '@/util/constants/size';
+
 export type IconName =
     | 'FillCaretRight'
     | 'OutlineClockCircle'
     | 'OutlineDoubleRight'
     | 'OutlineSearch'
     | 'OutlineTwitter';
+
+export type IconSize = keyof typeof ICON_SIZE;
