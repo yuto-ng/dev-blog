3a6fade53ee3ec168d6d5cbb8e475385de3a6731
y.nagayoshi <y.nagayoshi@scene-live.com>
2023-12-07 19:43:25 +0900

    [chore]lintエラー解消

--- next.config.js
+++ next.config.js
@@ -1,4 +1,4 @@
-import { createVanillaExtractPlugin } from '@vanilla-extract/next-plugin';
+const { createVanillaExtractPlugin } = require('@vanilla-extract/next-plugin');
 
 const nextConfig = {
     transpilePackages: ['@company/design-system'],
