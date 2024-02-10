4f4ed83da01dbc786f3c9b556188a07c27d8558e
y.nagayoshi <y.nagayoshi@scene-live.com>
2024-02-10 17:42:41 +0900

    [change]コンポーネント実装

--- src/components/ui-elements/Text/index.tsx
+++ src/components/ui-elements/Text/index.tsx
@@ -1,13 +1,29 @@
 import { ReactNode } from 'react';
 
-import { SampleStyle } from './style.css';
+import { TextStyleProps, textVariants, baseStyle } from './style.css';
 
 type Props = {
     children: ReactNode;
-};
+} & Partial<TextStyleProps>;
 
-const Text: React.FC<Props> = ({ children }) => {
-    return <div className={SampleStyle}>{children}</div>;
+const Text: React.FC<Props> = ({
+    children,
+    size = 'm',
+    lineHeight = 'm',
+    weight = 'normal',
+    color = 'lightBlack',
+}) => {
+    const getTextStyles = () => {
+        return [
+            baseStyle,
+            textVariants.size[size],
+            textVariants.color[color],
+            textVariants.weight[weight],
+            textVariants.lineHeight[lineHeight],
+        ].join(' ');
+    };
+
+    return <div className={getTextStyles()}>{children}</div>;
 };
 
 export default Text;
