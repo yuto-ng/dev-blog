94ed6e758ec9ba05c7ebae01db02885b09864f3a
y.nagayoshi <y.nagayoshi@scene-live.com>
2024-02-11 15:27:40 +0900

    [fix]調整

--- src/components/ui-elements/Icon/style.css.ts
+++ src/components/ui-elements/Icon/style.css.ts
@@ -2,7 +2,7 @@ import { style } from '@vanilla-extract/css';
 
 import { ICON_SIZE } from '@/constants';
 
-export const iconStyles = {
+export const container = {
     m: style({
         height: ICON_SIZE.m.height,
         width: ICON_SIZE.m.width,
