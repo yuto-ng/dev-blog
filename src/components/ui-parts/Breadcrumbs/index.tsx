4f82e9a4e6f5b75c903b2b07ec123c9cced68b14
y.nagayoshi <y.nagayoshi@scene-live.com>
2024-03-21 21:20:23 +0900

    [add]Breadcrumbs実装

---
+++ src/components/ui-parts/Breadcrumbs/index.tsx
@@ -0,0 +1,31 @@
+'use client';
+
+import { useSelectedLayoutSegments } from 'next/navigation';
+
+import * as style from './style.css';
+
+import Icon from '@/components/ui-elements/Icon';
+import LinkText from '@/components/ui-elements/LinkText';
+
+const Breadcrumbs = () => {
+    const segments = useSelectedLayoutSegments();
+    console.log(segments);
+
+    return (
+        <div className={style.container}>
+            <LinkText lineHeight="s" url="/" color="royalBlue">
+                ホーム
+            </LinkText>
+            <Icon iconName="FillCaretRight" size="s" />
+            <LinkText
+                lineHeight="s"
+                url={`/${segments[0]}/${segments[1]}/${segments[3]}`}
+                color="royalBlue"
+            >
+                カテゴリー
+            </LinkText>
+        </div>
+    );
+};
+
+export default Breadcrumbs;
