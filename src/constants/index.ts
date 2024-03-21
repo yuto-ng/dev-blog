3de561bb5a996f49ef736abf82ede1d7db3dc9eb
y.nagayoshi <y.nagayoshi@scene-live.com>
2024-03-21 22:43:55 +0900

    [fix]アイコンサイズ増やした

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,6 +1,7 @@
 export type IconSize = keyof typeof ICON_SIZE;
 
 export const ICON_SIZE = {
-    m: { height: '24px', width: '24px' },
+    l: { height: '24px', width: '24px' },
+    m: { height: '20px', width: '20px' },
     s: { height: '16px', width: '16px' },
 };
