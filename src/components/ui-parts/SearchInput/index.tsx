16e21396d527930bfd3de4bf715e3d7113ba1901
y.nagayoshi <y.nagayoshi@scene-live.com>
2024-03-21 22:44:07 +0900

    [add]SearchInput実装

---
+++ src/components/ui-parts/SearchInput/index.tsx
@@ -0,0 +1,20 @@
+import * as style from './style.css';
+
+import Icon from '@/components/ui-elements/Icon';
+
+type Props = {
+    handleClickSearchButton: () => void;
+};
+
+const SearchInput: React.FC<Props> = ({ handleClickSearchButton }) => {
+    return (
+        <div className={style.container}>
+            <input className={style.input} placeholder="検索" />
+            <button className={style.iconButton} onClick={handleClickSearchButton}>
+                <Icon iconName="OutlineSearch" size="m"></Icon>
+            </button>
+        </div>
+    );
+};
+
+export default SearchInput;
