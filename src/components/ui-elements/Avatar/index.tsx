73b1d64837631b16a53e3ecb34b70390bcef037c
Ynagayoshi <y.nagayoshi@scene-live.com>
2025-08-27 22:19:17 +0900

    不要な宣言削除

--- src/components/ui-elements/Avatar/index.tsx
+++ src/components/ui-elements/Avatar/index.tsx
@@ -10,7 +10,7 @@ function Avatar({ profileImageUrl }: Props) {
     const imageUrl = profileImageUrl ?? '/reactIcons/Avatar.svg';
 
     return (
-        <div className="relative h-[100px] w-[100px] rounded-full overflow-hidden">
+        <div className="relative h-[100px] w-[100px] overflow-hidden rounded-full">
             <Image src={imageUrl} alt="profile" fill />
         </div>
     );
