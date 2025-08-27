3a7ee4fcddcd47885095a14f9c4b98637005de3e
Ynagayoshi <y.nagayoshi@scene-live.com>
2025-08-27 23:02:15 +0900

    UI調整

--- src/components/ui-parts/Header/index.tsx
+++ src/components/ui-parts/Header/index.tsx
@@ -2,7 +2,7 @@ import Logo from '../Logo';
 
 function Header() {
     return (
-        <div>
+        <div className="h-20 bg-royalBlue flex justify-between items-center px-6">
             <Logo />
         </div>
     );
--- src/components/ui-parts/Logo/index.tsx
+++ src/components/ui-parts/Logo/index.tsx
@@ -1,7 +1,7 @@
 function Logo() {
     return (
-        <div>
-            <h3 className="text-white text-2xl font-bold">SB-engineer</h3>
+        <div className="bg-royalBlue border-4 border-white px-4 py-2 w-fit">
+            <h3 className="text-white text-2xl font-bold italic">SB-engineer</h3>
         </div>
     );
 }
